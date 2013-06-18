f3d14c60fd3791ed01cf6f39c4cc99ea70896037
alicem <alice.mottola@gmail.com>
2013-06-18 13:54:13 -0400

    Add a Lovecraft mad lib

---
+++ madlibs.rb
@@ -0,0 +1,69 @@
+puts "Let's butcher some Lovecraft."
+
+print "Plural noun: "
+plural_noun1 = gets.chomp
+
+print "Noun: "
+noun1 = gets.chomp
+
+print "Noun: "
+noun2 = gets.chomp
+
+print "Another noun: "
+noun3 = gets.chomp
+
+print "Plural noun: "
+plural_noun2 = gets.chomp
+
+print "Plural noun: "
+plural_noun3 = gets.chomp
+
+print "Noun: "
+noun4 = gets.chomp
+
+print "I'm sick of nouns, give me a past tense verb: "
+verb1 = gets.chomp
+
+print "Another past tense verb: "
+verb2 = gets.chomp
+
+print "Adjective: "
+adj1 = gets.chomp
+
+print "Noun: "
+noun5 = gets.chomp
+
+print "Adjective: "
+adj2 = gets.chomp
+
+print "Noun: "
+noun6 = gets.chomp
+
+print "Adjective: "
+adj3 = gets.chomp
+
+print "Last one! A noun: "
+noun7 = gets.chomp
+
+puts "Here's your journey out of oblivion...\n\n"
+
+print "When the last #{plural_noun1} were upon me, and the ugly
+#{noun1} of existence began to drive me to madness like
+the small drops of water that torturers let fall ceaselessly
+upon one spot of their victim's body, I loved the irradiate
+refuge of #{noun2}. In my dreams I found a little of the #{noun3}
+I had vainly sought in life, and wandered through old #{plural_noun2}
+and enchanted #{plural_noun3}.
+
+Once when the wind was soft and scented I heard the #{noun4}
+calling, and #{verb1} endlessly and languorously under strange
+stars.\n\n
+Once when the gentle rain fell I #{verb2} in a barge down a
+sunless stream under the earth till I reached another world
+of #{adj1} #{noun5}, #{adj2} #{noun6}, and #{adj3} #{noun7}.
+
+And once I walked through a golden valley that led to shadowy
+groves and ruins, and ended in a mighty wall green with
+antique vines, and pierced by a little gate of bronze.\n\n"
+
+puts "Look at you! You're a true Lovecraftian now!"
